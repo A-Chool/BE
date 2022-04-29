c92c1bfbc1a9bde81d6dd3508006bf12e9ee519d
hyun-woong <mr.woong0926@gmail.com>
2022-04-29 15:39:39 +0900

    유효성 검사 수정

--- src/main/java/com/RoutineGongJakSo/BE/security/validator/UserValidator.java
+++ src/main/java/com/RoutineGongJakSo/BE/security/validator/UserValidator.java
@@ -15,14 +15,14 @@ public class UserValidator {
 
     public static void checkUser(Optional<User> found, JoinDto joinDto) {
         Matcher match = Pattern.compile("(\\w)\\1\\1").matcher(joinDto.getUserPw());
-        if(match.find()){
-            throw new NullPointerException("비밀번호는 중복된 글자가 3개 미만이어야 합니다.");
-        }
+//        if(match.find()){
+//            throw new NullPointerException("비밀번호는 중복된 글자가 3개 미만이어야 합니다.");
+//        }
         if(found.isPresent()){
             throw new NullPointerException("아이디가 중복됩니다.");
         }
-        if(!joinDto.getUserPwCheck().equals(joinDto.getUserPw())){
-            throw new NullPointerException("비밀번호 확인이 다릅니다.");
-        }
+//        if(!joinDto.getUserPwCheck().equals(joinDto.getUserPw())){
+//            throw new NullPointerException("비밀번호 확인이 다릅니다.");
+//        }
     }
 }
