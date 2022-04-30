928f87780cedf3bdfc70f1270aca1697bfd6f4c8
hyun-woong <mr.woong0926@gmail.com>
2022-04-30 16:46:12 +0900

    feat: 권한 변경 및 유저정보 삭제 기능 추가

--- src/main/java/com/RoutineGongJakSo/BE/admin/AdminController.java
+++ src/main/java/com/RoutineGongJakSo/BE/admin/AdminController.java
@@ -15,13 +15,25 @@ public class AdminController {
 
     //관리자 로그인
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody AdminDto adminDto){
+    public Map<String, Object> login(@RequestBody AdminDto.RequestDto adminDto){
         return adminService.login(adminDto);
     }
 
-    //전체 유저 조회
-    @GetMapping("userList")
+    //전체 유저 조
+    @GetMapping("/userList")
     public List<User> getAllUser(){
         return adminService.getAllUser();
+    }
+
+    //권한 변경
+    @PutMapping("/{userId}")
+    public String updateLevel(@PathVariable Long userId, @RequestBody AdminDto.Update update){
+        return adminService.updateLevel(userId, update);
+    }
+
+    //유저 삭제
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable Long userId){
+        return adminService.deleteUser(userId);
     }
 }
--- src/main/java/com/RoutineGongJakSo/BE/admin/AdminDto.java
+++ src/main/java/com/RoutineGongJakSo/BE/admin/AdminDto.java
@@ -2,13 +2,20 @@ package com.RoutineGongJakSo.BE.admin;
 
 import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class AdminDto {
-    //로그인 request
-    private String email;
-    private String password;
+
+    @Getter
+    @Setter
+    //관리자 로그인 request
+    public static class RequestDto{
+        private String email;
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    //권한수정
+    public static class Update{
+        private int userLevel;
+    }
 }
