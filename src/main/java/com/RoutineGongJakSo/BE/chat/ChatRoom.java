af7671fdcdc7d493479694f2a2f7494a55162c60
hobit22 <hobit22@naver.com>
2022-04-30 17:53:08 +0900

    #1 feat STOMP pub/sub 추가중

--- src/main/java/com/RoutineGongJakSo/BE/chat/ChatRoom.java
+++ src/main/java/com/RoutineGongJakSo/BE/chat/ChatRoom.java
@@ -1,33 +1,20 @@
 package com.RoutineGongJakSo.BE.chat;
 
-import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.socket.WebSocketSession;
+import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.UUID;
 
 @Getter
+@Setter
 public class ChatRoom {
-    private final String roomId;
-    private final String name;
-    private final Set<WebSocketSession> sessions = new HashSet<>();
+    private String roomId;
+    private String name;
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
-    }
-
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
     }
 }
