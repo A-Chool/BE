501da76efa1eccffdd96bcafa70132878244880e
hobit22 <hobit22@naver.com>
2022-05-01 02:17:40 +0900

    #1 feat redis 연결 완료 * todo redis 에 채팅 저장

--- src/main/java/com/RoutineGongJakSo/BE/chat/ChatTestData.java
+++ src/main/java/com/RoutineGongJakSo/BE/chat/ChatTestData.java
@@ -6,20 +6,20 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ChatTestData implements ApplicationRunner {
-
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ChatRoomRepository chatRoomRepository;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        String roomName = "1조";
-        chatRoomRepository.createChatRoom(roomName);
-
-    }
-}
+//@Component
+//public class ChatTestData implements ApplicationRunner {
+//
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private ChatRoomRepository chatRoomRepository;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        String roomName = "1조";
+//        chatRoomRepository.createChatRoom(roomName);
+//
+//    }
+//}
