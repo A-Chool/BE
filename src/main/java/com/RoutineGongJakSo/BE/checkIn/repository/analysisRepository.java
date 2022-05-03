e8caf115752e72a59dfc4c735de0f7b5ae27e9ba
hyun-woong <mr.woong0926@gmail.com>
2022-05-04 02:45:54 +0900

    feat: CheckIn 관련 Repository 생성

---
+++ src/main/java/com/RoutineGongJakSo/BE/checkIn/repository/analysisRepository.java
@@ -0,0 +1,8 @@
+package com.RoutineGongJakSo.BE.checkIn.repository;
+
+import com.RoutineGongJakSo.BE.model.Analysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface analysisRepository extends JpaRepository<Analysis, Long> {
+
+}
