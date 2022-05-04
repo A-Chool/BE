2411a0fa121fae74052cf942c339149b398f1908
hyun-woong <mr.woong0926@gmail.com>
2022-05-05 01:30:18 +0900

    fix: 새벽 5시 기준으로 끊는거 GET 요청은 해치움

--- src/main/java/com/RoutineGongJakSo/BE/checkIn/CheckInService.java
+++ src/main/java/com/RoutineGongJakSo/BE/checkIn/CheckInService.java
@@ -13,6 +13,7 @@ import javax.transaction.Transactional;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -34,23 +35,32 @@ public class CheckInService {
     public String checkIn(UserDetailsImpl userDetails) {
         // 로그인 여부 확인
         validator.loginCheck(userDetails);
+
+        //현재 서울 날짜
+        String date = LocalDate.now(ZoneId.of("Asia/Seoul")).toString();
+
         // 유저 정보를 찾음
         User user = validator.userInfo(userDetails);
 
-        List<CheckIn> checkInList = checkInRepository.findByUser(user);
+        List<CheckIn> checkInList = checkInRepository.findByUserAndDate(user, date);
 
         for (CheckIn check : checkInList) {
             if (check.getCheckOut() == null) {
                 throw new NullPointerException("체크아웃을 먼저 해주세요");
             }
         }
-        //현재 서울 날짜
-        String date = LocalDate.now(ZoneId.of("Asia/Seoul")).toString();
+        //ToDo if(다음날 29시 보다 작다면, 날짜는 = 오늘 날짜로 ㄱㄱ)
+
         //현재 시간
         ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar tomorrow = Calendar.getInstance();
+//        tomorrow.setTime(sdf.parse(date));
+        tomorrow.add(Calendar.DATE, 1);
+        String strTomorrow = sdf.format(tomorrow.getTime());
 
-        log.info("현재 서울 날짜 : " + date);
-        log.info("현재 서울 시간 : " + nowSeoul);
+//        if (nowSeoul > 오늘 05시보다 작다면 date는 -1) {
+//        }
 
         CheckIn checkIn = CheckIn.builder()
                 .user(user)
@@ -72,8 +82,36 @@ public class CheckInService {
         //[서울]현재 날짜
         String date = LocalDate.now(ZoneId.of("Asia/Seoul")).toString();
 
-        //해당 유저의 해당 날짜의 전체 기록을 찾기
+        //내일 날짜를 구함
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar yesterDay = Calendar.getInstance();
+        yesterDay.setTime(sdf.parse(date)); //전날
+        yesterDay.add(Calendar.DATE, -1);
+        String strYesterDay = sdf.format(yesterDay.getTime());
+
+        SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss");
+        String sumTomorrow = strYesterDay + "05:00:00"; //전일 오전5시 기준
+        Date setFormatter = formatter2.parse(sumTomorrow);
+
+        //해당 유저의 해당 날짜의 전체 기록 찾기
         List<CheckIn> checkInList = checkInRepository.findByUserAndDate(user, date);
+
+        //해당 유저의 전날 날짜의 전체 기록 찾기
+        List<CheckIn> findCheckList = checkInRepository.findByUserAndDate(user, strYesterDay);
+
+        Calendar setFormat = Calendar.getInstance(); // 초기화 시간 05시
+        setFormat.setTime(setFormatter);
+
+        //현재시간보다 과거일 때,
+        for (CheckIn checkIn :  findCheckList){
+            String sumDateTime = checkIn.getDate() + checkIn.getCheckIn();
+            Date sumFormatter = formatter2.parse(sumDateTime); //체크인 시간
+            Calendar sumFormat = Calendar.getInstance(); //체크인 시간
+            sumFormat.setTime(sumFormatter);
+            if (setFormat.compareTo(sumFormat) < 0){ // 인자보다 과거일 경우
+                checkInList.add(checkIn); //해당 유저의 해당 날짜 전체 기록에 추가
+            }
+        }
 
         //기록 정보가 없을 때 return
         if (checkInList.size() < 1) {
@@ -90,11 +128,11 @@ public class CheckInService {
             throw new NullPointerException("아직 Start 를 누르지 않았습니다.");
         }
 
-        String[] test = lastCheckIn.getCheckIn().split(":"); //시, 분, 초 나누기
+        String[] timeStamp = lastCheckIn.getCheckIn().split(":"); //시, 분, 초 나누기
 
-        int HH = Integer.parseInt(test[0]); //시
-        int mm = Integer.parseInt(test[1]); //분
-        int ss = Integer.parseInt(test[2]); //초
+        int HH = Integer.parseInt(timeStamp[0]); //시
+        int mm = Integer.parseInt(timeStamp[1]); //분
+        int ss = Integer.parseInt(timeStamp[2]); //초
 
         //시간을 형식에 맞게 포맷
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss");
