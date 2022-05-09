758a6d02915c9e4d46532ce403d02f647f257aeb
hobit22 <hobit22@naver.com>
2022-05-09 17:30:37 +0900

    #64 refactoring ChatController

--- src/main/java/com/RoutineGongJakSo/BE/chat/controller/ChatController.java
+++ src/main/java/com/RoutineGongJakSo/BE/chat/controller/ChatController.java
@@ -2,10 +2,7 @@ package com.RoutineGongJakSo.BE.chat.controller;
 
 
 import com.RoutineGongJakSo.BE.chat.model.ChatMessage;
-import com.RoutineGongJakSo.BE.chat.pubsub.RedisPublisher;
-import com.RoutineGongJakSo.BE.chat.repo.ChatRoomRepository;
 import com.RoutineGongJakSo.BE.chat.service.ChatMessageService;
-import com.RoutineGongJakSo.BE.security.jwt.JwtDecoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Header;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -23,39 +19,14 @@ import java.util.List;
 @Controller
 public class ChatController {
 
-    private final RedisPublisher redisPublisher;
-    private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageService chatMessageService;
-    private final JwtDecoder jwtDecoder;
 
     /**
      * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
      */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message, @Header("Authorization") String token) {
-
-        // username 세팅
-        String username = "";
-        String sender = "";
-        if (!(String.valueOf(token).equals("Authorization") || String.valueOf(token).equals("null"))) {
-            String tokenInfo = token.substring(7); // Bearer빼고
-            username = jwtDecoder.decodeNickName(tokenInfo);
-            sender = jwtDecoder.decodeUsername(tokenInfo);
-        }
-        message.setSender(sender);
-        message.setNickname(username);
-
-        // 시간 세팅
-        Date date = new Date();
-        message.setCreatedAt(date.toString().substring(11, 19));
-        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-            chatRoomRepository.enterChatRoom(message.getRoomId());
-            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        } else {
-            chatMessageService.save(message);
-        }
-        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
-        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+        chatMessageService.save(message, token);
     }
 
     @GetMapping("/chat/message/{roomId}")
