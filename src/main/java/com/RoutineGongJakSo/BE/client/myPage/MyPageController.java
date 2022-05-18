dac5e9b43e33976c50c6cd26acd52f69b64bed2f
hyun-woong <mr.woong0926@gmail.com>
2022-05-18 18:41:43 +0900

    feat: 프로필 이미지 수정 완료

---
+++ src/main/java/com/RoutineGongJakSo/BE/client/myPage/MyPageController.java
@@ -0,0 +1,24 @@
+package com.RoutineGongJakSo.BE.client.myPage;
+
+import com.RoutineGongJakSo.BE.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class MyPageController {
+
+    private final MyPageService myPageService;
+
+    //프로필 이미지 수정
+    @PutMapping("/api/user/mypage/image")
+    public String updateImage(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                              @RequestPart("imageUrl") MultipartFile multipartFile){
+        return myPageService.updateImage(userDetails, multipartFile);
+
+    }
+}
