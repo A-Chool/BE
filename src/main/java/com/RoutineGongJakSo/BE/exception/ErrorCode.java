553eaa15e9314d6e3a0a31504992d276fb82b41f
hyun-woong <mr.woong0926@gmail.com>
2022-05-18 20:35:41 +0900

    fix: ERROR_CODE ADD

--- src/main/java/com/RoutineGongJakSo/BE/exception/ErrorCode.java
+++ src/main/java/com/RoutineGongJakSo/BE/exception/ErrorCode.java
@@ -13,12 +13,17 @@ public enum ErrorCode {
     BLANK_WEEK_NAME(HttpStatus.BAD_REQUEST, "400_2", "주차는 필수 입력값입니다."),
     DISPLAY_WEEK_ID(HttpStatus.BAD_REQUEST, "400_3", "이 주차는 현재 display 중이며, 지울 수 없습니다."),
     TOO_LONG_WEEK_NAME(HttpStatus.BAD_REQUEST, "400_4", "주차는 필수 입력값입니다."),
+    BAD_FORM_TYPE(HttpStatus.BAD_REQUEST, "400_5", "이미지 형식이 올바르지 않습니다."),
+    LIAR_USER_IMAGE(HttpStatus.BAD_REQUEST, "400_6", "업로드된 이미지가 존재하지 않습니다."),
 
     // 404 Not Found
     NOT_FOUND_WEEK_ID(HttpStatus.NOT_FOUND, "404_1", "주차 아이디가 존재하지 않습니다."),
     NOT_FOUND_USER_ID(HttpStatus.NOT_FOUND, "404_1", "유저 아이디가 존재하지 않습니다."),
     NOT_FOUND_LOCATION_ID(HttpStatus.NOT_FOUND, "404_2", "지역 코드가 존재하지 않습니다."),
-    NOT_FOUND_CATEGORY_ID(HttpStatus.NOT_FOUND, "404_2", "카테고리 코드가 존재하지 않습니다.")
+    NOT_FOUND_CATEGORY_ID(HttpStatus.NOT_FOUND, "404_2", "카테고리 코드가 존재하지 않습니다."),
+
+    // 500 Sever  Error
+    FAIL_FILE_UPLODA(HttpStatus.INTERNAL_SERVER_ERROR, "505_1", "파일 업로드에 실패 했습니다.")
     ;
 
     private final HttpStatus httpStatus;
