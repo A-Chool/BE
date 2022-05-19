d7a77f564ddd4fa56f3bffa57326480a07d16442
hyun-woong <mr.woong0926@gmail.com>
2022-05-19 18:26:26 +0900

    feat: RefreshToken JPA

--- src/main/java/com/RoutineGongJakSo/BE/client/refreshToken/RefreshTokenRepository.java
+++ src/main/java/com/RoutineGongJakSo/BE/client/refreshToken/RefreshTokenRepository.java
@@ -3,5 +3,6 @@ package com.RoutineGongJakSo.BE.client.refreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    RefreshToken findByRefreshToken(String token);
 
 }
