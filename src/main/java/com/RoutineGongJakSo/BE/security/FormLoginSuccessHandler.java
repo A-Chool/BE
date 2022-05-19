0be43c474c1727c39da44c29b527fa46d5ad1b74
hyun-woong <mr.woong0926@gmail.com>
2022-05-19 17:48:14 +0900

    feat: Refresh Token 발급 기능 추가

--- src/main/java/com/RoutineGongJakSo/BE/security/FormLoginSuccessHandler.java
+++ src/main/java/com/RoutineGongJakSo/BE/security/FormLoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.RoutineGongJakSo.BE.security;
 
 import com.RoutineGongJakSo.BE.security.jwt.JwtTokenUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
@@ -8,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // todo: FormLogin 이 성공했을 때 호출된다.
+@Slf4j
 public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     public static final String AUTH_HEADER = "Authorization";
+    public static final String REFRESH_HEADER = "RefreshAuthorization";
     public static final String TOKEN_TYPE = "BEARER";
 
     @Override
@@ -19,8 +22,11 @@ public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSucc
         final UserDetailsImpl userDetails = ((UserDetailsImpl) authentication.getPrincipal());
         // Token 생성
         final String token = JwtTokenUtils.generateJwtToken(userDetails);
-        System.out.println("token = " + token);
+        final String refreshToken = JwtTokenUtils.generateRefreshToken(userDetails.getUserEmail());
+        log.info("refreshToken : " + refreshToken);
+        log.info("accessToken : " + token);
         // todo: generateJwtToken - JWT 토큰이 만들어진다.
         response.addHeader(AUTH_HEADER, TOKEN_TYPE + " " + token);
+        response.addHeader(REFRESH_HEADER,TOKEN_TYPE + " " + refreshToken);
     }
 }
