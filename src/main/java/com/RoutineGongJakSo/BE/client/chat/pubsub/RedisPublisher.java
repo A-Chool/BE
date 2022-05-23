8de0af9de185f74f11303c7da24cd189022b9637
hobit22 <hobit22@naver.com>
2022-05-24 04:32:40 +0900

    feature/chat/HB 채팅 수정 완료!!!!

--- src/main/java/com/RoutineGongJakSo/BE/client/chat/pubsub/RedisPublisher.java
+++ src/main/java/com/RoutineGongJakSo/BE/client/chat/pubsub/RedisPublisher.java
@@ -2,17 +2,23 @@ package com.RoutineGongJakSo.BE.client.chat.pubsub;
 
 import com.RoutineGongJakSo.BE.client.chat.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class RedisPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void publish(ChannelTopic topic, ChatMessage message) {
+        log.info("publish");
+        log.info("topic : {}", topic.getTopic());
+        log.info("roomId : {}", message.getRoomId());
+        log.info("message : {}", message.getMessage());
         redisTemplate.convertAndSend(topic.getTopic(), message);
     }
 }
