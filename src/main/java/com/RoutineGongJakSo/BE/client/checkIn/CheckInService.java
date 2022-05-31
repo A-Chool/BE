a447982e298385a01ae2fc4ea0d9a21e1a1bc3f9
hobit22 <hobit22@naver.com>
2022-06-01 01:21:32 +0900

    feature/sse/HB timeOut 추가 및 sse 구현 완료

--- src/main/java/com/RoutineGongJakSo/BE/client/checkIn/CheckInService.java
+++ src/main/java/com/RoutineGongJakSo/BE/client/checkIn/CheckInService.java
@@ -97,7 +97,10 @@ public class CheckInService {
         checkInRepository.save(checkIn);
 
         log.info("체크인 {}", response);
-        sseController.publishCheckIn(checkIn);
+
+
+
+        sseController.publishCheckIn(checkIn, lateCheck(checkInList));
         return response;
     }
 
@@ -238,7 +241,7 @@ public class CheckInService {
             findAnalysis.get().setDaySum(daySum); // 총 공부 시간
 
             log.info("체크아웃 {}", getCheckIn(userDetails));
-
+            sseController.publishCheckOut(userDetails.getUser());
             return getCheckIn(userDetails);
         }
 
@@ -252,7 +255,6 @@ public class CheckInService {
         lastCheckIn.setAnalysis(analysis);
         analysisRepository.save(analysis);
 
-        log.info("체크아웃 {}", getCheckIn(userDetails));
         sseController.publishCheckOut(userDetails.getUser());
         return getCheckIn(userDetails);
     }
